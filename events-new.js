868a03f82d1b6acafc3dcefe02dbcb1b0c6c55c6
John Cherry <jac677@york.ac.uk>
2021-11-03 12:03:07 +0000

    style changes

--- events-new.js
+++ events-new.js
@@ -1,6 +1,6 @@
 let layout = `
-<div class="g-bg-gray-light-v5 ">
-    <section class="pt-3" ref="NewsTop" v-if="!groupid">
+<div>
+    <section class="pt-3 g-bg-gray-light-v5" ref="NewsTop" v-if="!groupid">
         <div class="contacts-wrapper">
             <div class="container">
                 <div class="row">
@@ -9,29 +9,34 @@ let layout = `
                             <h2 class="form__title h6">Filter Events</h2>
                             <div class="row">
                                 <div class="col-lg-3 pt-3">
-                                    <label class="g-font-weight-500 g-font-size-15 g-pl-30" for="event-category">Category</label>
-                                    <select id="event-category" class="form-select w-100 u-select-v2 u-shadow-v19 g-brd-none g-color-black g-color-primary--hover g-bg-white text-left g-rounded-30 g-pl-30 g-py-12" data-placeholder="All" data-open-icon="fa fa-angle-down" data-close-icon="fa fa-angle-up" @change="updateCategory($event)">
+                                    <label class="g-font-weight-600 g-font-size-15 g-pl-30" for="event-category">Category</label>
+                                    <select id="event-category" class="form-select g-brd-black g-brd-2 w-100 u-select-v2 u-shadow-v19 g-color-black g-color-primary--hover g-bg-white text-left g-rounded-30 g-pl-30 g-py-12" data-placeholder="All" data-open-icon="fa fa-angle-down" data-close-icon="fa fa-angle-up" @change="updateCategory($event)">
                                     <option value="">All</option>
                                     <option v-for="category in Categories" :value="category.id">{{ category.name }}</option>
                                     </select>
                                 </div>
                                 <div class="col-lg-3 pt-3">
-                                    <label class="g-font-weight-500 g-font-size-15 g-pl-30" for="event-activity">Activity</label>
-                                    <select id="event-activity" class="form-select w-100 u-select-v2 u-shadow-v19 g-brd-none g-color-black g-color-primary--hover g-bg-white text-left g-rounded-30 g-pl-30 g-py-12" data-placeholder="All" data-open-icon="fa fa-angle-down" data-close-icon="fa fa-angle-up" @change="updateGroup($event)">
+                                    <label class="g-font-weight-600 g-font-size-15 g-pl-30" for="event-activity">Activity</label>
+                                    <select id="event-activity" class="form-select g-brd-black g-brd-2 g-brd-2 w-100 u-select-v2 u-shadow-v19 g-color-black g-color-primary--hover g-bg-white text-left g-rounded-30 g-pl-30 g-py-12" data-placeholder="All" data-open-icon="fa fa-angle-down" data-close-icon="fa fa-angle-up" @change="updateGroup($event)">
                                     <option value="">All</option>
                                     <option v-for="activity in Groups" :value="activity.id">{{ activity.name }}</option>
                                     </select>
                                 </div>
                                 <div class="col-lg-3 pt-3">
-                                    <label class="g-font-weight-500 g-font-size-15 g-pl-30" for="event-search" >Search</label>
-                                    <input id="event-search" class="form-control u-shadow-v19 g-brd-none g-bg-white g-font-size-16 g-rounded-30 g-px-30 g-py-13 g-mb-30" aria-label="Search" type="text" name="search" placeholder="Search..." :value=Search @submit.prevent @keyup="search($event)"/>
+                                    <label class="g-font-weight-600 g-font-size-15 g-pl-30" for="event-search" >Search</label>
+                                    <div class="input-group mb-3">
+                                        <input id="event-search" class="form-control u-shadow-v19 g-brd-black g-brd-2 g-bg-white g-font-size-16 g-rounded-left-30 g-px-30 g-py-13 g-mb-30" aria-label="Search" type="text" name="search" placeholder="Search..." :value=Search />
+                                        <div class="input-group-append">
+                                            <button type="submit" class="btn btn-block u-shadow-v32 g-brd-black g-brd-2 g-color-black g-color-white--hover g-bg-transparent g-bg-black--hover g-font-size-16 g-rounded-right-30 g-px-20 g-py-13 g-mb-30" aria-label="Submit"><i class="fas fa-search"></i></button>
+                                        </div>
+                                    </div>
                                 </div>
                                 <div class="col-lg-2 pt-3 mt-3 d-none d-md-block">
-                                    <button class="btn btn-block u-shadow-v32 g-brd-black g-brd-2 g-color-black g-color-white--hover g-bg-transparent g-bg-black--hover g-font-size-16 g-rounded-30 g-py-10 mr-2 g-mt-0" type="button" @submit.prevent @click="changeView()" v-if="Cards">List View</button>
-                                    <button class="btn btn-block u-shadow-v32 g-brd-black g-brd-2 g-color-black g-color-white--hover g-bg-transparent g-bg-black--hover g-font-size-16 g-rounded-30 g-py-10 mr-2 g-mt-0" type="button" @submit.prevent @click="changeView()" v-else>Card View</button>
+                                    <button class="btn btn-block u-shadow-v32 g-brd-black g-brd-2 g-color-black g-color-white--hover g-bg-transparent g-bg-black--hover g-font-size-16 g-rounded-30 g-py-10 mr-2 g-mt-20 g-mb-30" type="button" @submit.prevent @click="changeView()" v-if="Cards"><i class="fas fa-list"></i> List View</button>
+                                    <button class="btn btn-block u-shadow-v32 g-brd-black g-brd-2 g-color-black g-color-white--hover g-bg-transparent g-bg-black--hover g-font-size-16 g-rounded-30 g-py-10 mr-2 g-mt-20 g-mb-30" type="button" @submit.prevent @click="changeView()" v-else><i class="fas fa-address-card"></i> Card View</button>
                                 </div>
                                 <div class="col-lg-1 pt-3 mt-3">
-                                    <button class="btn btn-block u-shadow-v32 g-brd-black g-brd-2 g-color-black g-color-white--hover g-bg-transparent g-bg-black--hover g-font-size-16 g-rounded-30 g-py-10 mr-2 g-mt-0" type="button" @submit.prevent @click="reset()">Reset</button>
+                                    <button class="btn btn-block u-shadow-v32 g-brd-black g-brd-2 g-color-black g-color-white--hover g-bg-transparent g-bg-black--hover g-font-size-16 g-rounded-30 g-py-10 mr-2 g-mt-20 g-mb-30" type="button" @submit.prevent @click="reset()">Reset</button>
                                 </div>
                             </div>
                         </form>
@@ -41,7 +46,7 @@ let layout = `
         </div>
     </section>
     <!-- Actual Events -->
-    <section class="section blog background--brown" @scroll="onScroll">
+    <section class="section blog background--brown pt-4" @scroll="onScroll">
         <div class="container">
             <div class="row">
                 <div class="col g-mb-30">
@@ -52,14 +57,16 @@ let layout = `
                         <div class="row justify-content-center">
                             <div class="col-md-3 my-3" v-for="event in Events">
                                 <div class="card">
-                                    <div class="card-header h5 text-center">
-                                        <time :datetime="event.start_date">{{ event.start_date | getDate }}</time>
+                                    <div class="card-header h5 text-center" style="color:black !important;"><em>
+                                        <time :datetime="event.start_date">{{ event.start_date | getDate }}</time></em>
                                     </div>
                                     <img v-if=event.thumbnail_url class="card-img-top"  :src=event.thumbnail_url alt="" />
                                     <img v-else-if="event.group && event.group.thumbnail_url" class="card-img-top" :src=event.group.thumbnail_url alt="" />
                                     <img v-else class="card-img-top" src="https://d350x4n02brjm.cloudfront.net/sums/website/images/500x500_Placeholder.jpg" alt="" />
                                     <div class="card-body text-center">
-                                        <h2 class="g-color-gray-dark-v2 h5 card-title" v-html="event.title"></h2>
+                                        <a :href="'/events/id/' + event.event_id + '-' + event.url_name">
+                                            <h2 class="g-color-gray-dark-v2 h5 card-title" v-html="event.title"></h2>
+                                        </a>
                                         <p class="card-text"><small>Start times from {{ event.start_date | getTime }}<span v-if=event.venue> | {{ event.venue.name }}</span></small></p>
                                         <p class="card-text">{{ event.short_description }}</p>
                                         <div class="text-center" v-if=event.external_tickets>
@@ -68,7 +75,7 @@ let layout = `
                                             Tickets</a>
                                         </div>
                                         <div class="text-center" v-else>
-                                            <a class="btn btn-xl u-btn-primary rounded-0 text-uppercase g-font-weight-700 g-font-size-12 g-mr-5 g-mt-10"
+                                            <a class="btn btn-xl u-btn-primary rounded-0 g-font-weight-700 g-font-size-12 g-mr-5 g-mt-10 text-white"
                                             :href="'/events/id/' + event.event_id + '-' + event.url_name">Tickets</a>
                                         </div>
                                     </div>
@@ -128,7 +135,7 @@ let layout = `
                 </div>
             </div>
             <div class="row d-flex justify-content-center m-3" v-if="MoreResults">
-                <button type="button" class="btn btn-xl u-btn-primary rounded-0 text-uppercase g-font-weight-700 g-font-size-12 g-mr-5 g-mt-10" @click="moreEvents()">Load More <i class="fa fa-chevron-down"></i></button>
+                <button type="button" class="btn btn-xl u-btn-secondary rounded-0 text-uppercase g-font-weight-700 g-font-size-12 g-mr-5 g-mt-10" @click="moreEvents()">Load More <i class="fa fa-chevron-down"></i></button>
             </div>
         </div>
     </section>
@@ -203,7 +210,7 @@ Vue.component('events', {
          */
         getEvents: function(append = false) {
 
-            let parameters = 'sortBy=start_date&onlyFuture=1&perPage=12&page=' + this.Page;
+            let parameters = 'sortBy=start_date&futureOrOngoing=1&perPage=12&page=' + this.Page;
             //add relevant parameters to the event search
             if (this.SelectedType) {
                 parameters += '&typeId=' + this.SelectedType;
@@ -267,7 +274,7 @@ Vue.component('events', {
         },
         reset() {
             //easy way to refresh the page
-            location.reload()
+            window.location = window.location.href.split("?")[0];
         },
         moreEvents() {
             this.Page++;
