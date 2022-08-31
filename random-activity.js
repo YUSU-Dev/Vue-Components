34f3818a221786c8f36a57131e4650aea1657509
Nathan Billis <nathanbillis.nathan@gmail.com>
2022-08-31 14:01:31 +0100

    Tidy file a bit

--- random-activity.js
+++ random-activity.js
@@ -1,8 +1,8 @@
-let parameters = 'sortBy=random&perPage=1&categoryIds=4,5,6,12,7,8,27,9,10,11,1,3'
+const parameters = 'sortBy=random&perPage=1&categoryIds=4,5,6,12,7,8,27,9,10,11,1,3'
 axios.get('https://pluto.sums.su/api/groups?' + parameters, {
-    headers: {
-        'X-Site-Id': "tZyLG9BX9f4hdTp2HLva5c"
-    }
+  headers: {
+    'X-Site-Id': 'tZyLG9BX9f4hdTp2HLva5c'
+  }
 }).then(function (response) {
-    window.location.replace('https://yusu.org/activities/view/' + response.data.data[0].url_name)
+  window.location.replace('https://yusu.org/activities/view/' + response.data.data[0].url_name)
 })
