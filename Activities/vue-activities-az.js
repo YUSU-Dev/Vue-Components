d336ed8f1ca81bb22cffc6f1736448fb6b4f584b
John Cherry <jac677@york.ac.uk>
2023-06-02 12:25:11 +0100

    Update vue-activities-az.js

--- Activities/vue-activities-az.js
+++ Activities/vue-activities-az.js
@@ -1,4 +1,4 @@
-let layout = `
+let layoutActivities = `
   <div class="row societies-a-z-block p-2" id="societies-a-z">
     <div class="col-12">
       <div class="row activities-list justify-content-center">
@@ -18,7 +18,7 @@ let layout = `
         </div>
         <div class="col-12 d-none d-lg-block" v-if="!Search && ParentCategories.length > 0">
           <div class="nav-societies-type text-center">
-            <p class="h3">Filters</p>
+            <p class="h3 g-mt-20">Filters</p>
             <ul class="clubs-nav-block nav nav-tabs nav-justified u-nav-v2-1 u-nav-rounded-3 u-nav-primary g-mb-30" data-btn-classes="btn btn-md btn-block u-btn-outline-primary g-mb-30">
               <li v-for="Parent in ParentCategories" @click.prevent="SelectedParent = Parent; SelectedCategory = ''; getGroups();" class="nav-item">
                 <a v-bind:class="{'active':(SelectedParent.id === Parent.id)}" class="nav-link">
@@ -49,26 +49,8 @@ let layout = `
             <div>
               <a :href="'/activities/view/' + Activity.url_name">
                 <div>
-                  <div v-if="Activity.thumbnail_url" 
-                       class="d-none d-md-block justify-content-center" 
-                       style="height: 9em;overflow:hidden; background-position: center; background-repeat: no-repeat; background-size: contain; cursor:pointer;" 
-                       v-bind:style="'background-image:url(' + Activity.thumbnail_url + ');'"
-                       v-bind:alt="Activity.name + ' Logo'" />
-                  <div v-else class="d-none d-md-block justify-content-center" 
-                       style="height: 9em;overflow:hidden ;background-image:url('https://yusu.s3.eu-west-2.amazonaws.com/sums/website/images/placeholder-events.png');
-                              background-position: center; background-repeat: no-repeat; background-size: contain; cursor:pointer;" 
-                       alt="Yusu Activities Logo"/>
-                  <div v-if="Activity.thumbnail_url" 
-                       class="d-md-none justify-content-center" 
-                       style="height: 9em;overflow:hidden; background-position: center; background-repeat: no-repeat; background-size: contain; cursor:pointer;" 
-                       v-bind:style="'background-image:url(' + Activity.thumbnail_url + ');'"
-                       v-bind:alt="Activity.name + ' Logo'" />
-                  <div v-else 
-                       class="d-md-none justify-content-center" 
-                       style="height: 5em;overflow:hidden ;background-image:url('https://yusu.s3.eu-west-2.amazonaws.com/sums/website/images/placeholder-events.png');
-                              background-position: center; background-repeat: no-repeat; background-size: contain; cursor:pointer;" 
-                       alt="Yusu Activities Logo" />
-
+                  <img v-if=Activity.thumbnail_url class="card-img-top" :src=Activity.thumbnail_url alt="" class="d-md-block justify-content-center" style="height: 9em;overflow:hidden; background-position: center; background-repeat: no-repeat; background-size: contain; cursor:pointer; object-fit: contain;" />
+                  <img v-else class="card-img-top" src="https://assets-cdn.sums.su/YU/IMG/YUSU_logo_single.png" alt="" class="d-none d-md-block justify-content-center" style="height: 9em;overflow:hidden; background-position: center; background-repeat: no-repeat; background-size: contain; cursor:pointer;" />
                 </div>
                 <div class="h6 g-color-grey g-mb-5 text-center align-bottom btn-block">
                   <p class="g-color-black">{{ Activity.name }}</p>
@@ -84,133 +66,123 @@ let layout = `
       </div>
     </div>
   </div>
-`
-
-Vue.component('VActivitiesAZ', {
-    template: layout,
-    props: ['siteid', 'selectedparents', 'title', 'selectedcategory'],
-    data() {
-        return {
-            Categories: [],
-            CategoryIDs: "",
-            ParentCategories: [],
-            Groups: [],
-            SelectedCategory: "",
-            SelectedParent: "",
-            SelectedParents: [],
-            Search: '',
-            Page: 1,
-            MoreResults: false,
-        }
-    },
-    created() {
-        var self = this;
-        if (self.selectedparents) {
-          self.SelectedParents = self.selectedparents.split(",");
-        } else if (self.selectedcategory) {
-          self.CategoryIDs = self.selectedcategory;
-        } else {
-          self.SelectedParents = "2,24";
-        }
-        
-        if (!self.title){
-          self.title = "Clubs and Societies: A-Z";
-        }
-        //check if looking for a specific activity, search, etc...
-        let urlParams = new URLSearchParams(window.location.search);
-        if (urlParams.has('search')) {
-            self.Search = urlParams.get('search');
-        }
-        //if we already have a category, don't get more info
-        if (!self.selectedcategory) {
-          //Get parents
-          axios.get('https://pluto.sums.su/api/groups/categories?sortBy=name&isParent=1', {
-              headers: {
-                  'X-Site-Id': self.siteid
-              }
-          }).then(function (response) {
-              response.data.forEach(category => {
-                  if (self.SelectedParents.includes(category.id.toString())) {
-                      self.ParentCategories = [...self.ParentCategories, category];
-                  }
-              });
-          });
-          //get categories
-          axios.get('https://pluto.sums.su/api/groups/categories?sortBy=name&isParent=0&parentIds=' + self.selectedparents , {
-              headers: {
-                  'X-Site-Id': self.siteid
-              }
-          }).then(function (response) {
-              self.Categories = response.data;
-              let idArray = self.Categories.map(function(item) {
-                return item['id'];
-              });
-              self.CategoryIDs = idArray.join();
-              self.getGroups();
-          });
-        } else {
-          self.getGroups();
-        }
-    },
-    methods: {
-        /**
-         * Fetch groups from API
-         * @param bool append - are we getting more groups to append to the current list?
-         */
-        getGroups: function (append = false) {
-            let self = this;
-            if (!append) { self.Page = 1; }
-            let parameters = 'sortBy=name&perPage=20&page=' + self.Page;
-            //add relevant parameters to the group search
-            if (self.CategoryIDs) {
-              parameters += '&categoryIds=' + self.CategoryIDs;
-            }
-            if (self.Search) {
-                parameters += '&searchTerm=' + self.Search;
-                self.SelectedCategory = self.SelectedParent = "";
-            } else if (self.SelectedCategory) {
-                parameters += '&categoryId=' + self.SelectedCategory.id;
-            } else if (self.SelectedParent) {
-                parameters += '&parentCategoryId=' + self.SelectedParent.id;
-            }
-            axios.get('https://pluto.sums.su/api/groups?' + parameters, {
-                headers: {
-                    'X-Site-Id': self.siteid
-                }
-            }).then(function (response) {
-                //if we want more events (append = true), add to array
-                if (append) {
-                    self.Groups = [...self.Groups, ...response.data.data];
-                } else {
-                    //otherwise replace current events
-                    self.Groups = response.data.data;
-                }
-                //If the API says there are more results (ie another page), update the template accordingly
-                if (response.data.next_page_url) {
-                    self.MoreResults = true
-                } else {
-                    self.MoreResults = false
-                }
-            })
-        },
-        moreGroups() {
-            this.Page++;
-            this.getGroups(true);
-        },
-        search(event) {
-            this.Search = event.target.value
-            this.getGroups();
-        },
-    },
-    computed: {
-        filteredCategories() {
-            let self = this;
-            return this.Categories.filter(category => {
-                if (self.SelectedParent) {
-                    return category.parent_id == self.SelectedParent.id
-                }
-
-            });
-        }
+`;
+Vue.component("VActivitiesAZ", {
+  template: layoutActivities,
+  props: ["siteid", "selectedparents", "title", "selectedcategory"],
+  data() {
+    return {
+      Categories: [],
+      CategoryIDs: "",
+      ParentCategories: [],
+      Groups: [],
+      SelectedCategory: "",
+      SelectedParent: "",
+      SelectedParents: [],
+      Search: "",
+      Page: 1,
+      MoreResults: false,
+    };
+  },
+  created() {
+    var self = this;
+    if (self.selectedparents) {
+      self.SelectedParents = self.selectedparents.split(",");
+    } else if (self.selectedcategory) {
+      self.CategoryIDs = self.selectedcategory;
+    } else {
+      self.SelectedParents = "2,24";
     }
+    if (!self.title) {
+      self.title = "Clubs and Societies: A-Z";
+    }
+    let urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.has("search")) {
+      self.Search = urlParams.get("search");
+    }
+    if (!self.selectedcategory) {
+      axios
+        .get(
+          "https://pluto.sums.su/api/groups/categories?sortBy=name&isParent=1",
+          { headers: { "X-Site-Id": self.siteid } }
+        )
+        .then(function (response) {
+          response.data.forEach((category) => {
+            if (self.SelectedParents.includes(category.id.toString())) {
+              self.ParentCategories = [...self.ParentCategories, category];
+            }
+          });
+        });
+      axios
+        .get(
+          "https://pluto.sums.su/api/groups/categories?sortBy=name&isParent=0&parentIds=" +
+            self.selectedparents,
+          { headers: { "X-Site-Id": self.siteid } }
+        )
+        .then(function (response) {
+          self.Categories = response.data;
+          let idArray = self.Categories.map(function (item) {
+            return item["id"];
+          });
+          self.CategoryIDs = idArray.join();
+          self.getGroups();
+        });
+    } else {
+      self.getGroups();
+    }
+  },
+  methods: {
+    getGroups: function (append = false) {
+      let self = this;
+      if (!append) {
+        self.Page = 1;
+      }
+      let parameters = "sortBy=name&perPage=20&page=" + self.Page;
+      if (self.CategoryIDs) {
+        parameters += "&categoryIds=" + self.CategoryIDs;
+      }
+      if (self.Search) {
+        parameters += "&searchTerm=" + self.Search;
+        self.SelectedCategory = self.SelectedParent = "";
+      } else if (self.SelectedCategory) {
+        parameters += "&categoryId=" + self.SelectedCategory.id;
+      } else if (self.SelectedParent) {
+        parameters += "&parentCategoryId=" + self.SelectedParent.id;
+      }
+      axios
+        .get("https://pluto.sums.su/api/groups?" + parameters, {
+          headers: { "X-Site-Id": self.siteid },
+        })
+        .then(function (response) {
+          if (append) {
+            self.Groups = [...self.Groups, ...response.data.data];
+          } else {
+            self.Groups = response.data.data;
+          }
+          if (response.data.next_page_url) {
+            self.MoreResults = true;
+          } else {
+            self.MoreResults = false;
+          }
+        });
+    },
+    moreGroups() {
+      this.Page++;
+      this.getGroups(true);
+    },
+    search(event) {
+      this.Search = event.target.value;
+      this.getGroups();
+    },
+  },
+  computed: {
+    filteredCategories() {
+      let self = this;
+      return this.Categories.filter((category) => {
+        if (self.SelectedParent) {
+          return category.parent_id == self.SelectedParent.id;
+        }
+      });
+    },
+  },
 });
